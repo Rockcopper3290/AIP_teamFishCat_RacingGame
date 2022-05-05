65efc46f98b17ad044014b82801c463b77c30c66
Example Name <Brendon.Kitcher@mds.torrens.edu.au>
2022-05-05 10:37:07 +1000

    CP navigation / Simple AI created and moving -Brendon
    Both AI and player will automatically navigate the track.
    Looks quite messy at the moment, have tried to comment the things that will be removed/ moved once the players car is instantiated properly.
    Getting the AI cars and player car to instantiate correctly shouldn't be that big of a deal.

--- Assets/Scripts/FishCat_Player.cs
+++ Assets/Scripts/FishCat_Player.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class FishCat_Player : MonoBehaviour
 {
     Vector3 playerVelocity;
+    Vector3 vectorLeft;
+    Vector3 vectorToNextCP;
 
-    public float playerAngle = 0.0f;
-    float playerTurnSpeed = 180.0f;
+    float playerAngle = 0.0f;
+    float vectorLeftAngle = 0.0f;
+    float angleToNode = 0.0f;
+    public int nodeCounter = 1;
+    public float playerTurnSpeed = 180.0f;
     public float playerSpeed = 1.0f;
 
-    Rigidbody2D playerRigidBody;
+    bool sWasPressed = false;
 
-    // Update is called on the first frame
+    Rigidbody2D playerRigidBody;
+    public GameObject nextCP;
+
+    // Update is called on the first frame ----- CHANGE TO AWAKE OR SOMETHING ONC THE CARS GET INSTANTIATED
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
+        vectorLeftAngle = playerAngle + 90.0f;
 
         AngleToVectorMovement();
     }
@@ -23,27 +32,66 @@ public class FishCat_Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // DELETE THIS LATER, ONCE THE CARS GET INSTANTIATED {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            sWasPressed = true; 
+            nextCP = GameObject.Find("Node_" + nodeCounter);
+        }
+        if (sWasPressed) 
+        {
+            vectorToNextCP = nextCP.GetComponent<Node_Checkpoint>().nodePosition - transform.position;
+            // DELETE THIS LATER, ONCE THE CARS GET INSTANTIATED } 
+
+
         playerVelocity.Normalize();
         playerVelocity *= playerSpeed;
         playerRigidBody.AddForce(playerVelocity, ForceMode2D.Force);
-
         playerRigidBody.rotation = playerAngle;
 
-        if (Input.GetKey(KeyCode.A))
-        {
+        // Calculates whether to turn left or right
+        angleToNode = Vector3.Angle(vectorLeft, vectorToNextCP);
+
+        if (angleToNode <= 90)  // Turns left
+            {
             playerAngle += playerTurnSpeed * Time.deltaTime;
+            vectorLeftAngle = playerAngle + 90.0f;
             AngleToVectorMovement();
         }
-        else if (Input.GetKey(KeyCode.D))
-        {
+        else if (angleToNode > 90)  // Turns right
+            {
             playerAngle -= playerTurnSpeed * Time.deltaTime;
+            vectorLeftAngle = playerAngle + 90.0f;
             AngleToVectorMovement();
         }
+
+        // Increments to the next CP upon getting close enough to the current CP
+        if (Vector3.Magnitude(vectorToNextCP) < 0.3f)
+        {
+            ++nodeCounter;
+
+            // Increments through checkpoints 
+            if (GameObject.Find("Node_" + nodeCounter) != null) nextCP = GameObject.Find("Node_" + nodeCounter);
+            else
+            {
+                nodeCounter = 0;
+                nextCP = GameObject.Find("Node_0");
+            }
+        }
+        
+
+        
+            playerSpeed = 0.2f; // DELETE THIS LATER, ONCE THE CARS GET INSTANTIATED {
+        } // }
     }
 
-    // Takes playerAngle and turns it into a vector/ velocity for movement
+    // Takes fishCatAngle and turns it into a vector/ velocity for movement
+    // Calculates the players left into a vector
+    // Keeps fishCatAngle and fishCatLeft between 0-360
     void AngleToVectorMovement()
     {
         playerVelocity = new Vector3(Mathf.Cos(playerAngle * Mathf.Deg2Rad), Mathf.Sin(playerAngle * Mathf.Deg2Rad));
+
+        vectorLeft = new Vector3(Mathf.Cos(vectorLeftAngle * Mathf.Deg2Rad), Mathf.Sin(vectorLeftAngle * Mathf.Deg2Rad));
     }
 }
