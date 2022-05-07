c8d38d8d4f6e90570f9432a76bc783bd013232e9
Example Name <Brendon.Kitcher@mds.torrens.edu.au>
2022-05-08 09:35:26 +1000

    Revert "oil part 2 (electric boogaloo)"
    This reverts commit 5f6f95801c0892fd72232fb93076e34cd49cfda2.

--- Assets/Scripts/Obstacle_Collider.cs
+++ Assets/Scripts/Obstacle_Collider.cs
@@ -4,27 +4,8 @@ using UnityEngine;
 
 public class Obstacle_Collider : MonoBehaviour
 {
-    public FishCat_Player fishCatPlayer;
-    float normalPlayerSpeed;
-
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-        normalPlayerSpeed = fishCatPlayer.playerSpeed;
-        if (other.tag == "Oil")
-        {
-            Debug.Log("Oil detected");
-            fishCatPlayer.playerSpeed = (normalPlayerSpeed/3);
-
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "Oil")
-        {
-            Debug.Log("No longer in Oil");
-            fishCatPlayer.playerSpeed = normalPlayerSpeed;
-        }
+        Debug.Log("Oil detected");
     }
 }
